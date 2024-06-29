5c2701be306a115ec49ffd74a91194aa85eeab81
Thomas Csigai <thomascsigai1@gmail.com>
2024-06-29 15:33:04 +0200

    Installing ImGui

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,10 @@
 ﻿#include <glew.h>
 #include <glfw3.h>
 #include <glm.hpp>
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+
 
 #include <iostream>
 
@@ -33,11 +37,34 @@ int main(void)
     glm::vec3 v(1.0f, 0.0f, 0.0f);
     std::cout << "GLM vector: (" << v.x << ", " << v.y << ", " << v.z << ")" << std::endl;
 
+    // Initialize ImGui
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+
+    ImGui::StyleColorsDark();
+
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 130");
+
     // render loop --------------------------------------
     while (!glfwWindowShouldClose(window))
     {
         glClear(GL_COLOR_BUFFER_BIT);
 
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        // Create a simple ImGui window
+        ImGui::Begin("Hello, world!");
+        ImGui::Text("This is some text.");
+        ImGui::End();
+
+        ImGui::Render();
+        glClearColor(0.45f, 0.55f, 0.60f, 1.00f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
         glfwSwapBuffers(window);
         glfwPollEvents();
