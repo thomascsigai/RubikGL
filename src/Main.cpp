50ce91140c4f61325f068529d73678a31445f345
Thomas Csigai <thomascsigai1@gmail.com>
2024-06-29 12:26:00 +0200

    Installing glew

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,21 +1,32 @@
-﻿#include <glfw3.h>
+﻿#include <glew.h>
+#include <glfw3.h>
+
+#include <iostream>
 
 int main(void)
 {
     GLFWwindow* window;
 
-    if (!glfwInit())
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
         return -1;
+    }
 
-    // create the window --------------------------------
     window = glfwCreateWindow(1280, 720, "RubikGL", NULL, NULL);
     if (!window)
     {
+        std::cerr << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
     }
 
     glfwMakeContextCurrent(window);
+
+    GLenum err = glewInit();
+    if (err != GLEW_OK) {
+        std::cerr << "Failed to initialize GLEW: " << glewGetErrorString(err) << std::endl;
+        return -1;
+    }
 
     // render loop --------------------------------------
     while (!glfwWindowShouldClose(window))
